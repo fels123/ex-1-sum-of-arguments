c371dc567eb4acc745f0dd8bbb62aa387a33ec9e
fels123 <kelengulienakhro@gmail.com>
2024-06-29 22:23:09 +0530

    handing error messages using try catch method

--- practice.js
+++ practice.js
@@ -18,14 +18,17 @@
 // console.log(sum([3, 6, 1]));
 
 // ***********************************************************
-//area of a circle
-//circle.raius =2
-//circle.area = 20;
+// area of a circle
+// circle.raius = 2;
+// circle.area = 20;
 
+// calculating area of a circle
 // const circles = function (radius) {
 //   return Math.PI * radius * radius;
 // };
 // console.log(circles(4));
+
+// ************************************
 
 // const circle = {
 //   radius: 4,
@@ -39,3 +42,21 @@
 // Use circle object with area getter if you prefer encapsulation, object-oriented design, and readability, especially in scenarios involving multiple properties or methods related to circles.
 
 // ************************************************************
+//ERROR HANDLING
+
+try {
+  const numbers = [1, 2, 3, 4];
+
+  const count = countOccurances(numbers, 1);
+  console.log(count);
+} catch (e) {
+  console.log(e.message);
+}
+
+function countOccurances(array, searchElement) {
+  if (!Array.isArray(array)) throw new Error("Invalid array");
+  return array.reduce((accumulator, current) => {
+    const occurance = current === searchElement ? 1 : 0;
+    return accumulator + occurance;
+  }, 0);
+}
