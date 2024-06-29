3f0b49b882f4b5360ae44476262f045bc40bbb34
fels123 <kelengulienakhro@gmail.com>
2024-06-29 22:05:29 +0530

    calcualting area of a circle the easy way and using get method

--- practice.js
+++ practice.js
@@ -3,16 +3,39 @@
 // create function called sum taking
 //  varying arguments which will be =10
 
-function sums(...items) {
-  return items.reduce((a, b) => a + b);
-}
-console.log(sums(3, 6, 1));
+// function sums(...items) {
+//   return items.reduce((a, b) => a + b);
+// }
+// console.log(sums(3, 6, 1));
 
-// to challenge yourself modify the
-// sum to take an array and still give the same result
+// // to challenge yourself modify the
+// // sum to take an array and still give the same result
 
-function sum(...items) {
-  if (items.length === 1 && Array.isArray(items[0])) items = [...items[0]];
-  return items.reduce((a, b) => a + b);
-}
-console.log(sum([3, 6, 1]));
+// function sum(...items) {
+//   if (items.length === 1 && Array.isArray(items[0])) items = [...items[0]];
+//   return items.reduce((a, b) => a + b);
+// }
+// console.log(sum([3, 6, 1]));
+
+// ***********************************************************
+//area of a circle
+//circle.raius =2
+//circle.area = 20;
+
+// const circles = function (radius) {
+//   return Math.PI * radius * radius;
+// };
+// console.log(circles(4));
+
+// const circle = {
+//   radius: 4,
+//   get area() {
+//     return Math.PI * this.radius * this.radius;
+//   },
+// };
+// console.log(circle.area);
+
+// conclusion:Use circles function if you prioritize reusability and simplicity for standalone calculations.
+// Use circle object with area getter if you prefer encapsulation, object-oriented design, and readability, especially in scenarios involving multiple properties or methods related to circles.
+
+// ************************************************************
